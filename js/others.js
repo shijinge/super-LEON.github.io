5de7090afbf3eec536b17a07551fbb620ab0229f
super-LEON <supergg008@foxmail.com>
2016-07-09 20:38:15 +0800

    完成选项卡切换动画，修复初次加载闪现320px小窗bug，解决3D图片环切换时执行问题，增加选项卡切换时box更换背景

--- js/others.js
+++ js/others.js
@@ -1,253 +1,245 @@
-(function(){
 
-	var M=11;
-	var oDiv=document.getElementById('div1');
-	var aDiv=oDiv.getElementsByClassName('hid');
-	var oCard4 = document.querySelector('.card4');
-	var aHead = document.querySelectorAll('.ahead li');
+//初次加载自适应屏幕，重要！！！
+var oWarp = document.querySelector('.warp');
+oWarp.style.height = document.documentElement.clientHeight + 'px';
 
-	var x=-10;
-	var y=0;
+document.documentElement.style.fontSize = 32 / 320 * document.documentElement.clientWidth + 'px';
+var isReady = true;
+function movePic() {
 
-	var speedX=0;
-	var speedY=0;
+	if (isReady) {
+		isReady = false;
+		var M = 11;
+		var oDiv = document.getElementById('div1');
+		var aDiv = oDiv.getElementsByClassName('hid');
+		var oCard4 = document.querySelector('.card4');
+		var aHead = document.querySelectorAll('.ahead li');
 
-	var oWarp = document.querySelector('.warp');
+		var x = -10;
+		var y = 0;
 
-	oWarp.style.height = document.documentElement.clientHeight + 'px';
+		var speedX = 0;
+		var speedY = 0;
 
-	document.documentElement.style.fontSize = 32 / 320 * document.documentElement.clientWidth + 'px';
+		for (var i = 1; i <= M; i++) {
+			var oNewDiv = document.createElement('div');
 
+			oNewDiv.className = 'hid';
 
-	for(var i=1;i<=M;i++)
-	{
-		var oNewDiv=document.createElement('div');
+			(function (oNewDiv, i) {
+				setTimeout(function () {
+					oNewDiv.style.WebkitTransform = 'rotateY(' + (360 * (i - 1) / M) + 'deg) translateZ(400px)';
+					oNewDiv.style.MozTransform = 'rotateY(' + (360 * (i - 1) / M) + 'deg) translateZ(400px)';
+					oNewDiv.style.msTransform = 'rotateY(' + (360 * (i - 1) / M) + 'deg) translateZ(400px)';
+					oNewDiv.style.OTransform = 'rotateY(' + (360 * (i - 1) / M) + 'deg) translateZ(400px)';
+					oNewDiv.style.transform = 'rotateY(' + (360 * (i - 1) / M) + 'deg) translateZ(400px)';
 
-		oNewDiv.className='hid';
+					setTimeout(function () {
+						if (i == M)fixAll();
 
-			(function (oNewDiv,i){
-				setTimeout(function (){
-					oNewDiv.style.WebkitTransform='rotateY('+(360*(i-1)/M)+'deg) translateZ(400px)';
-					oNewDiv.style.MozTransform='rotateY('+(360*(i-1)/M)+'deg) translateZ(400px)';
-					oNewDiv.style.msTransform='rotateY('+(360*(i-1)/M)+'deg) translateZ(400px)';
-					oNewDiv.style.OTransform='rotateY('+(360*(i-1)/M)+'deg) translateZ(400px)';
-					oNewDiv.style.transform='rotateY('+(360*(i-1)/M)+'deg) translateZ(400px)';
+						setTimeout(function () {
 
-					setTimeout(function (){
-						if(i==M)fixAll();
+							oNewDiv.style.WebkitTransition = 'none';
+							oNewDiv.style.MozTransition = 'none';
+							oNewDiv.style.msTransition = 'none';
+							oNewDiv.style.OTransition = 'none';
+							oNewDiv.style.transition = 'none';
 
-						setTimeout(function (){
-
-							oNewDiv.style.WebkitTransition='none';
-							oNewDiv.style.MozTransition='none';
-							oNewDiv.style.msTransition='none';
-							oNewDiv.style.OTransition='none';
-							oNewDiv.style.transition='none';
-
-						},1000);
+						}, 1000);
 					}, 3000);
-				}, (M+3-i)*200);
-			})(oNewDiv,i);
+				}, (M + 3 - i) * 200);
+			})(oNewDiv, i);
 
 
-
-		oNewDiv.degY=360*(i-1)/M;
-		var oContent1 = '<div class="img">';
-		var oContent2 = '<span class="over"><span class="shadow"></span></span></div>';
-		switch (i) {
-			case 1:
-				oNewDiv.innerHTML=oContent1 + '<a href="www.baidu.com" class="link">点此预览</a>'+oContent2;
-				break;
-			case 0:
-				oNewDiv.innerHTML='<div class="img">111111<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 2:
-				oNewDiv.innerHTML='<div class="img">333333<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 3:
-				oNewDiv.innerHTML='<div class="img">4444<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 4:
-				oNewDiv.innerHTML='<div class="img">555555<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 5:
-				oNewDiv.innerHTML='<div class="img">666666<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 6:
-				oNewDiv.innerHTML='<div class="img">77777<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 7:
-				oNewDiv.innerHTML='<div class="img">88888<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 8:
-				oNewDiv.innerHTML='<div class="img">999999<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 9:
-				oNewDiv.innerHTML='<div class="img">1010<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 10:
-				oNewDiv.innerHTML='<div class="img">11 11 11<span class="over"><span class="shadow"></span></span></div>';
-				break;
-			case 11:
-				oNewDiv.innerHTML='<div class="img">121212<span class="over"><span class="shadow"></span></span></div>';
-				break;
-		}
-		/*oNewDiv.innerHTML='<div class="img">1111111111<span class="over"><span class="shadow"></span></span></div>';*/
-
+			oNewDiv.degY = 360 * (i - 1) / M;
+			var oContent1 = '<div class="img">';
+			var oContent2 = '<span class="over"><span class="shadow"></span></span></div>';
+			switch (i) {
+				case 1:
+					oNewDiv.innerHTML = oContent1 + '<a href="www.baidu.com" class="link">点此预览</a>' + oContent2;
+					break;
+				case 0:
+					oNewDiv.innerHTML = '<div class="img">111111<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 2:
+					oNewDiv.innerHTML = '<div class="img">333333<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 3:
+					oNewDiv.innerHTML = '<div class="img">4444<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 4:
+					oNewDiv.innerHTML = '<div class="img">555555<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 5:
+					oNewDiv.innerHTML = '<div class="img">666666<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 6:
+					oNewDiv.innerHTML = '<div class="img">77777<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 7:
+					oNewDiv.innerHTML = '<div class="img">88888<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 8:
+					oNewDiv.innerHTML = '<div class="img">999999<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 9:
+					oNewDiv.innerHTML = '<div class="img">1010<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 10:
+					oNewDiv.innerHTML = '<div class="img">11 11 11<span class="over"><span class="shadow"></span></span></div>';
+					break;
+				case 11:
+					oNewDiv.innerHTML = '<div class="img">121212<span class="over"><span class="shadow"></span></span></div>';
+					break;
+			}
+			/*oNewDiv.innerHTML='<div class="img">1111111111<span class="over"><span class="shadow"></span></span></div>';*/
 
 
-
-		oNewDiv=oNewDiv.getElementsByClassName('img')[0];
-
+			oNewDiv = oNewDiv.getElementsByClassName('img')[0];
 
 
-		oNewDiv.style.background='url(images/img2/'+i+'.jpg)';
+			oNewDiv.style.background = 'url(images/img2/' + i + '.jpg)';
 
-		oNewDiv.getElementsByClassName('shadow')[0].style.background='-webkit-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/'+i+'.jpg)';
-		oNewDiv.getElementsByClassName('shadow')[0].style.background='-moz-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/'+i+'.jpg)';
-		oNewDiv.getElementsByClassName('shadow')[0].style.background='-ms-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/'+i+'.jpg)';
-		oNewDiv.getElementsByClassName('shadow')[0].style.background='-o-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/'+i+'.jpg)';
-		oNewDiv.getElementsByClassName('shadow')[0].style.backgroundSize='100% 100%';
-		oNewDiv.style.backgroundSize='100% 100%';
+			oNewDiv.getElementsByClassName('shadow')[0].style.background = '-webkit-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/' + i + '.jpg)';
+			oNewDiv.getElementsByClassName('shadow')[0].style.background = '-moz-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/' + i + '.jpg)';
+			oNewDiv.getElementsByClassName('shadow')[0].style.background = '-ms-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/' + i + '.jpg)';
+			oNewDiv.getElementsByClassName('shadow')[0].style.background = '-o-linear-gradient(rgba(0,0,0,1) 40%, rgba(255,255,255,0)), url(images/img2/' + i + '.jpg)';
+			oNewDiv.getElementsByClassName('shadow')[0].style.backgroundSize = '100% 100%';
+			oNewDiv.style.backgroundSize = '100% 100%';
 
-		oDiv.appendChild(oNewDiv.parentNode);
+			oDiv.appendChild(oNewDiv.parentNode);
 
-	}
-
-	var aImg = document.querySelectorAll('.img');
-	for (var i = 0; i < aImg.length; i++) {
-		aImg[i].onmouseover = function () {
-			this.style.WebkitTransform = 'scale(1.3,1.3)';
-			this.style.MozTransform='scale(1.3,1.3)';
-			this.style.msTransform='scale(1.3,1.3)';
-			this.style.OTransform='scale(1.3,1.3)';
-			this.style.transform='scale(1.3,1.3)';
-		};
-		aImg[i].onmouseout = function () {
-			this.style.WebkitTransform = 'scale(1,1)';
-			this.style.MozTransform='scale(1,1)';
-			this.style.msTransform='scale(1,1)';
-			this.style.OTransform='scale(1,1)';
-			this.style.transform='scale(1,1)';
-		};
-		aImg[i].children[0].onclick = function(ev) {
-			alert(1);
-			var ev = ev ||event;
-			ev.cancelBubble = true;
 		}
 
+		var aImg = document.querySelectorAll('.img');
+		for (var i = 0; i < aImg.length; i++) {
+			aImg[i].onmouseover = function () {
+				this.style.WebkitTransform = 'scale(1.3,1.3)';
+				this.style.MozTransform = 'scale(1.3,1.3)';
+				this.style.msTransform = 'scale(1.3,1.3)';
+				this.style.OTransform = 'scale(1.3,1.3)';
+				this.style.transform = 'scale(1.3,1.3)';
+			};
+			aImg[i].onmouseout = function () {
+				this.style.WebkitTransform = 'scale(1,1)';
+				this.style.MozTransform = 'scale(1,1)';
+				this.style.msTransform = 'scale(1,1)';
+				this.style.OTransform = 'scale(1,1)';
+				this.style.transform = 'scale(1,1)';
+			};
+			aImg[i].children[0].onclick = function (ev) {
+				alert(1);
+				var ev = ev || event;
+				ev.cancelBubble = true;
+			}
 
 
-	oCard4.onmousedown=function (ev)
-	{
+			oCard4.onmousedown = function (ev) {
 
-		var oEvent=ev||event;
-		var mouseStartX=oEvent.clientX;
-		var mouseStartY=oEvent.clientY;
+				var oEvent = ev || event;
+				var mouseStartX = oEvent.clientX;
+				var mouseStartY = oEvent.clientY;
 
-		var startX=x;
-		var startY=y;
+				var startX = x;
+				var startY = y;
 
-		var lastX=mouseStartX;
-		var lastY=mouseStartY;
+				var lastX = mouseStartX;
+				var lastY = mouseStartY;
 
-		speedX=speedY=0;
+				speedX = speedY = 0;
 
-		document.onmousemove=function(ev)
-		{
-			var oEvent=ev||event;
+				document.onmousemove = function (ev) {
+					var oEvent = ev || event;
 
-			y=startY+(oEvent.clientX-mouseStartX)/10;
-			x=startX-(oEvent.clientY-mouseStartY)/10;
+					y = startY + (oEvent.clientX - mouseStartX) / 10;
+					x = startX - (oEvent.clientY - mouseStartY) / 10;
 
-			speedX=(oEvent.clientX-lastX)/5;
-			speedY=(oEvent.clientY-lastY)/5;
+					speedX = (oEvent.clientX - lastX) / 5;
+					speedY = (oEvent.clientY - lastY) / 5;
 
-			fixAll();
+					fixAll();
 
-			lastX=oEvent.clientX;
-			lastY=oEvent.clientY;
-		};
+					lastX = oEvent.clientX;
+					lastY = oEvent.clientY;
+				};
 
-		document.onmouseup=function (ev)
-		{
+				document.onmouseup = function (ev) {
 
 
-			document.onmousemove=null;
-			document.onmouseup=null;
+					document.onmousemove = null;
+					document.onmouseup = null;
 
-			startMove();
-		};
+					startMove();
+				};
 
-		stopMove();
-
-		return false;
-	};
-
-	var timer=null;
-	function startMove()
-	{
-		clearInterval(timer);
-		timer=setInterval(function (){
-			x-=speedY;
-			y+=speedX;
-
-			speedY*=0.93;
-			speedX*=0.93;
-
-			if(Math.abs(speedX)<0.1 && Math.abs(speedY)<0.1)
 				stopMove();
 
-			fixAll();
-		}, 30);
-	}
+				return false;
+			};
 
-	function stopMove()
-	{
-		clearInterval(timer);
-	}
+			var timer = null;
 
+			function startMove() {
+				clearInterval(timer);
+				timer = setInterval(function () {
+					x -= speedY;
+					y += speedX;
 
+					speedY *= 0.93;
+					speedX *= 0.93;
 
+					if (Math.abs(speedX) < 0.1 && Math.abs(speedY) < 0.1)
+						stopMove();
 
-		//aImg[i].style.href = 'www.baidu.com';
-		/*if(Math.abs(speedX)<0.2 && Math.abs(speedY)<0.2){
-			aImg[i].onclick = function () {
-				alert(1);
+					fixAll();
+				}, 30);
 			}
-		}*/
 
-	}
+			function stopMove() {
+				clearInterval(timer);
+			}
 
-	function fixAll()
-	{
-		oDiv.style.WebkitTransform='perspective(1000px) rotateX('+x+'deg) rotateY('+y+'deg)';
-		oDiv.style.MozTransform='perspective(1000px) rotateX('+x+'deg) rotateY('+y+'deg)';
-		oDiv.style.msTransform='perspective(1000px) rotateX('+x+'deg) rotateY('+y+'deg)';
-		oDiv.style.OTransform='perspective(1000px) rotateX('+x+'deg) rotateY('+y+'deg)';
-		oDiv.style.transform='perspective(1000px) rotateX('+x+'deg) rotateY('+y+'deg)';
 
-		for(var i=0;i<aDiv.length;i++)
-		{
-			var deg=aDiv[i].degY+y;
-			var a=(deg%360+360)%360;
+			//aImg[i].style.href = 'www.baidu.com';
+			/*if(Math.abs(speedX)<0.2 && Math.abs(speedY)<0.2){
+			 aImg[i].onclick = function () {
+			 alert(1);
+			 }
+			 }*/
 
-			a=Math.abs(180-a);
-
-			var d=0.1+(a/180)*0.9;
-
-			if(d<0.2)d=0.2;
-
-			aDiv[i].style.opacity=d;
-
-			//aDiv[i].innerHTML=parseInt(a);
 		}
+
+		function fixAll() {
+			oDiv.style.WebkitTransform = 'perspective(1000px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+			oDiv.style.MozTransform = 'perspective(1000px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+			oDiv.style.msTransform = 'perspective(1000px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+			oDiv.style.OTransform = 'perspective(1000px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+			oDiv.style.transform = 'perspective(1000px) rotateX(' + x + 'deg) rotateY(' + y + 'deg)';
+
+			for (var i = 0; i < aDiv.length; i++) {
+				var deg = aDiv[i].degY + y;
+				var a = (deg % 360 + 360) % 360;
+
+				a = Math.abs(180 - a);
+
+				var d = 0.1 + (a / 180) * 0.9;
+
+				if (d < 0.2)d = 0.2;
+
+				aDiv[i].style.opacity = d;
+
+				//aDiv[i].innerHTML=parseInt(a);
+			}
+		}
+
+
+	} else {
+		return;
 	}
 
-
-
+}
 	//首屏canvas画心
-	
+(function(){
 	var arr = [];//保存所有的XY坐标，只为验证。实际程序中可删除。
 	var r = 2;
 	var radian;//弧度
@@ -294,7 +286,6 @@
 	function getY(t) {//由弧度得到 Y 坐标
 		return 50 - r * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
 	}
-//canvas画心结束
-
-	
 })();
+
+//canvas画心结束
