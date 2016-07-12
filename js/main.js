5ef9ea68ebaffe63cd9566ecfe7f9ad68aaa2e72
super-LEON <supergg008@foxmail.com>
2016-07-12 09:41:40 +0800

    首页文字top改为百分比设置，修复首页出现滚动条bug

--- js/main.js
+++ js/main.js
@@ -7,17 +7,17 @@ window.onload = function () {
     var aHead = document.querySelectorAll('.ahead li');
     var aContent = document.querySelectorAll('.box >div');
     var aCardtext = document.querySelectorAll('.ahead li a');
-    var aSkTxt = document.querySelectorAll('.sk-box span');
-    for(var i = 0; i < aSkTxt.length; i++) {
-        aSkTxt[i].style.lineHeight = aSkTxt[0].parentNode.parentNode.offsetHeight;
-    }
+    var aSkill = document.querySelectorAll('.card2 .sk-box li');
+
+
     var iNow= 0;
     var bOk = true;
     //console.log(aContent);
 
 
-
 //初次加载自适应屏幕
+    //changeClient();
+
     window.onresize = changeClient;
 
 //窗口大小改变自适应屏幕
@@ -99,6 +99,26 @@ window.onload = function () {
                 });
                 break;
             case 1:
+                //给技能描述遮罩设定初始位置
+                //由于在其他选项卡显示时，选项卡2状态为隐藏，无法获取oS的宽度和高度，所以加在此处，顺便给所有的遮罩加运动
+                for (var i = 0; i < aSkill.length; i++) {
+                    var oS = aSkill[i].children[1];
+                     oS.style.left = -oS.offsetWidth + 'px';
+                     oS.style.top = -oS.offsetHeight + 'px';
+
+                    through(aSkill[i]);
+                }
+
+                /*var aC = document.querySelectorAll('.card2 canvas');
+
+                for(var i = 0; i < aC.length; i++) {
+                    aC[i].width = aC[i].parentNode.offsetWidth;
+                    aC[i].height = aC[i].parentNode.offsetHeight;
+
+                }*/
+
+
+
                 move(aContent[iNow], {top: 0, left: 0}, {duration: 500,
                     easing: Tween.Sine.easeIn,
                     complete: function () {
@@ -177,6 +197,103 @@ window.onload = function () {
     }
 
 //选项卡2
+
+    //遮罩运动
+
+    //三角函数计算判断位置
+    function a2d(n) {
+        return n * 180/Math.PI;
+    }
+    function hoverDir(obj,ev){
+        var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+        var w = obj.offsetWidth;
+        var h = obj.offsetHeight;
+        var x = obj.offsetLeft+w/2-ev.clientX;
+        var y = obj.offsetTop+h/2-scrollTop-ev.clientY;
+        //计算公式
+        return Math.round((a2d(Math.atan2(y,x))+180)/90)%4;
+    }
+
+    function through(obj) {
+        var oS = obj.children[1];
+        //鼠标移入的时候
+        obj.onmouseover = function (ev) {
+            var oEvent = ev || event;
+            var oForm = oEvent.fromElement || oEvent.relatedTarget;
+            //处理onmouseover的bug
+            if (this.contains(oForm))return;
+            var dir = hoverDir(this, oEvent);
+            //判断移入方向，设置初始位置
+            switch (dir) {
+                case 0:
+                    //右
+                    oS.style.left = oS.offsetWidth + 'px';
+                    //alert(oS.offsetWidth);
+                    oS.style.top = 0;
+                    break;
+                case 1:
+                    //下
+                    oS.style.left = 0;
+                    oS.style.top = oS.offsetHeight + 'px';
+                    break;
+                case 2:
+                    //左
+                    oS.style.left = -oS.offsetWidth + 'px';
+                    oS.style.top = 0;
+                    break;
+                case 3:
+                    //上
+                    oS.style.left = 0;
+                    oS.style.top = -oS.offsetHeight + 'px';
+                    break;
+            }
+            //运动目标点（0,0）
+
+            move(oS, {'top': 0, 'left': 0});
+        };
+        //鼠标移出的时候
+        obj.onmouseout = function (ev) {
+            var oEvent = ev || event;
+            var oTo = oEvent.toElement || oEvent.relatedTarget;
+            //处理onmouseout的bug
+            if (this.contains(oTo))return;
+            var dir = hoverDir(obj, oEvent);
+            //判断移出方向，运动至目标点
+            switch (dir) {
+                case 0:
+                   // alert('r');
+                    //右
+                    move(oS, {left: oS.offsetWidth, top: 0});
+                    break;
+                case 1:
+                    //alert('r')
+                    //下
+                    move(oS, {left: 0, top: oS.offsetHeight});
+                    break;
+                case 2:
+                    //alert('r')
+                    //左
+                    move(oS, {left: -oS.offsetWidth, top: 0});
+                    break;
+                case 3:
+                    //alert('r')
+                    //上
+                    move(oS, {left: 0, top: -oS.offsetHeight});
+                    break;
+            }
+        };
+    }
+    //以上调用在选项卡的case1里
+
+/*
+    var aC = document.querySelectorAll('.card2 canvas');
+    for(var i = 0; i < aC.length; i++) {
+        aC[i].width = 0;
+        aC[i].height = 0;
+        alert(aC[i].width);
+
+    }*/
+
 
 
 
